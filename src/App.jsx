4fe1f944cc682f4feabaf5615030ef9c31ab0ce6
Nini <94174963+ninni13@users.noreply.github.com>
2025-09-10 15:12:07 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import LoginPage from './pages/LoginPage.jsx'
 import DiaryPage from './pages/DiaryPage.jsx'
 import SettingsPage from './pages/SettingsPage.jsx'
 import TrashPage from './pages/TrashPage.jsx'
-import SentimentTestPage from './pages/SentimentTestPage.jsx'
 
 export default function App() {
   return (
@@ -13,7 +12,6 @@ export default function App() {
         <Route path="/" element={<DiaryPage />} />
         <Route path="/trash" element={<TrashPage />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/sentiment-test" element={<SentimentTestPage />} />
       </Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
